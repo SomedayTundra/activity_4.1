e81597c708635d9f90142874dc8726d0cda83e0d
SomedayTundra <markstracka@gmail.com>
2025-07-01 14:19:02 -0500

    got things working with four separate components, one each for add, subtract, mult, and div; go things to lay out nicely using book author's App.css, with the key being consistent widths on comps that will end up in the same columns of a flex layout

---
+++ src/components/Add.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Add() {
+    const [operands, setOperands] = useState({ first: 0, second: 0 });
+
+    function updateFirstNumber(event) {
+        setOperands({
+            first: +event.target.value,
+            second: operands.second
+        });
+    }
+
+    function updateSecondNumber(event) {
+        setOperands({
+            first: operands.first,
+            second: +event.target.value
+        })
+    }
+
+    const result = operands.first + operands.second;
+
+    return (
+        <p>
+            <span id="mathtype">ADD</span>
+            <input type="number" onChange={updateFirstNumber} /> +{' '}
+            <input type="number" onChange={updateSecondNumber}/> = {result}
+        </p>
+    );
+}
+
+export default Add;
